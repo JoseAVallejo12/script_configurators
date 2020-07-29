19cf584bfd880feda089b26a7c33a2e310e89c2c
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-29 10:15:38 -0500

    autogenerate list of tool work done

--- a_config.sh
+++ a_config.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 #----- program list for be install in the run script-----------
-PROGRAM_LIST=(
-	betty
-	git
-	git_config
-	shellcheck
-	valgrind
-	mysql
-	vim
-	emacs
-	zsh
-)
+PROGRAM_LIST=()
+
+#-----function for auto generate list of tool contained in scripts_install dir ----
+function get_tool_list()
+{
+	idx=0
+	for tool_path in ./assets/scripts_install/*
+	do
+		tool_name="$(basename "$tool_path" | tr '.' '\n' | head -n 1)"
+		PROGRAM_LIST=("${PROGRAM_LIST[@]}" "$tool_name")
+		idx+=1
+
+	done
+}
+
 #--------auto generate var prompt dict (only for bash v4.0.0 or high------------
 declare -A PROMP_DICT
 for i in "${PROGRAM_LIST[@]}"
@@ -24,12 +28,7 @@ for i in "${PROGRAM_LIST[@]}"
 do
 	CTRL_DICT["$i"]+="NA"
 done
-
 # function that handles command-not-found message.
-command_not_found_handle()
-{
-return 127;
-}
 
 # ---------user prompt message.------------
 prompt()
@@ -64,14 +63,14 @@ prompt()
 	echo '';
 }
 
-# color settings
+#--------- color settings for promp funtion --------------
 color_settings()
 {
 	RED="\033[0;31m"; GREEN="\033[0;32m"; CYAN="\033[0;36m";
 	YELLOW="\033[1;33m"; WHITE="\033[1;37m"; NC="\033[0m";
 }
 
-# installed programs checker.
+#------ installed programs checker.---------------------
 function prog_validator()
 {
 	RED="\033[0;31m"
@@ -81,7 +80,7 @@ function prog_validator()
 
 	for ((i=0; i<"$len"; i++))
 	do
-		tool="${PROGRAM_LIST[i]}"
+		tool="${PROGRAM_LIST[$i]}"
 		tool_stat="${CTRL_DICT["$tool"]}"
 		if [ "$tool_stat" != "SKIPPED" ]
 		then
@@ -104,7 +103,7 @@ function prog_validator()
 	done
 }
 
-# 0. SUDO Install.
+#------- checker if sudo is in pc else it install ------
 function install_sudo()
 {
 	if [ -e "$(which "sudo")" ];
@@ -115,6 +114,7 @@ function install_sudo()
 	fi
 }
 
+#---- Funtion for handle install of all tool that user need ------
 function install_tools()
 {
 	if [ "${CTRL_DICT["$1"]}" != "SKIPPED" ]
@@ -140,58 +140,13 @@ function install_tools()
 	fi
 }
 
-# 4. git_credentials installation.
-function install_git_config()
-{
-	if [ "${CTRL_DICT["git_config"]}" != "SKIPPED" ];
-	then
-		echo 'Install git credential helper ? (y/n)';
-		read -r VAR1_GITCRED;
-		if [ "$VAR1_GITCRED" == "y" ];
-		then
-			install_sudo;
-			wait;
-			echo "";
-			echo -e "*Please write your github ${GREEN}USER NAME${NC} account:";
-			read -r GIT_USER;
-			echo "";
-			echo -e "*Please write your github ${GREEN}PASSWORD${NC} account:";
-			read -r GIT_PASSW;
-			echo "";
-			echo -e "*Please write your github ${GREEN}EMAIL${NC} account:";
-			read -r GIT_EMAIL;
-			# ----------------------------------------------------------------
-			echo "[user]" > "$HOME"/.gitconfig
-			echo "     name = $GIT_USER" >> "$HOME"/.gitconfig
-			echo "     email = $GIT_EMAIL" >> "$HOME"/.gitconfig
-			echo "[push]" >> "$HOME"/.gitconfig
-			echo "     default = matching" >> "$HOME"/.gitconfig
-			echo "[credential]" >> "$HOME"/.gitconfig
-			echo "     helper = store" >> "$HOME"/.gitconfig
-			# ----------------------------------------------------------------
-			echo "https://$GIT_USER:$GIT_PASSW@github.com" >> "$HOME"/.git-credentials
-			return;
-
-			clear;
-			echo "**************************************";
-			echo "   Github Credentials installing...   ";
-			echo "**************************************";
-			sleep 2;
-		elif [ "$VAR1_GITCRED" == "n" ];
-		then
-			CTRL_DICT["git_config"]="SKIPPED"
-			PROMP_DICT["git_config"]="${CYAN} SKIPPED ${NC}";
-		fi
-	fi
-}
-
-
 # -----------------------------------------
 # main program of the script (entry point).
 # -----------------------------------------
 # ------ Autogenerate prompt -------------------------------------------------
-color_settings;
-prog_validator;
+get_tool_list
+color_settings
+prog_validator
 prompt
 
 len="${#PROGRAM_LIST[@]}"
