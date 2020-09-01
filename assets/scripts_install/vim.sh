a3e8ffc8011f5069fe6982ec6f82ded60901d973
Jose A. Vallejo <54599840+josevallejo1984@users.noreply.github.com>
2020-08-31 23:22:51 -0500

    create script for vim install

--- assets/scripts_install/vim.sh
+++ assets/scripts_install/vim.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "this escrips is for vim instalations"
+# This escrips is for vim instalations
+sudo apt-get update -y
+sudo apt-get install -y vim
+wait
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+wait
+sudo rm "$HOME/.vimrc"
+sudo cp "assets/scripts_config/.vimrc" "$HOME/.vimrc"
+sudo vim +PluginInstall +qall
