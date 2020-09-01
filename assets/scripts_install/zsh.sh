59fcdcf1d8323fa7b2002408ede632d542481d57
Jose A. Vallejo <54599840+josevallejo1984@users.noreply.github.com>
2020-08-31 23:20:35 -0500

    script for install zsh done

--- assets/scripts_install/zsh.sh
+++ assets/scripts_install/zsh.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "this escrips is for zsh instalations"
+# Escrips is for zsh instalations
+sudo apt-get update -y
+wait
+sudo apt-get install -y zsh
+wait
+git clone https://github.com/ohmyzsh/ohmyzsh.git
+wait
+chmod u+x "$PWD"/oh-my-zsh/tools/install.sh;
+clear
