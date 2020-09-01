d78dd47072498b77b280e08ff8355c3c0d4c1969
Jose A. Vallejo <54599840+josevallejo1984@users.noreply.github.com>
2020-08-31 23:16:31 -0500

    update comments

--- assets/scripts_install/asdf.sh
+++ assets/scripts_install/asdf.sh
@@ -1,8 +1,12 @@
+#!/usr/bin/env bash
+# Manage multiple runtime versions with a single CLI tool
 sudo apt-get update
 sudo apt install -y curl git
+wait
 
 # *******Install dependencias tested in ubuntu 18.04******
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0-rc1
+wait
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libsqlite3-dev
 sudo apt-get install -y libssl1.0
@@ -26,5 +30,7 @@ asdf update
 
 # ***** Add plugin python and nodejs ******
 asdf plugin add python
+wait
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+wait
 bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
