584a5ec091bed83e53ebdedaae2cce355f10578b
Jose A. Vallejo <54599840+josevallejo1984@users.noreply.github.com>
2020-09-03 14:48:32 -0500

    add step for install mysql 5.7.x version

--- assets/scripts_install/mysql.sh
+++ assets/scripts_install/mysql.sh
@@ -3,3 +3,11 @@
 sudo apt-get update -y
 wait
 sudo apt-get install -y mysql-server;
+
+
+#******* for install version 5.7.x
+# echo 'deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7-dmr' | sudo tee -a /etc/apt/sources.list
+#sudo apt-get update
+#sudo apt-get install mysql-server-5.7
+#wait
+#mysql --version
